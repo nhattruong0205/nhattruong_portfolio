53a02fcb2935737e463471bf0d02cc9654845f6e
nhattruong0205 <123410500+nhattruong0205@users.noreply.github.com>
2026-03-11 20:40:12 -0500

    add Iris Project Info and fix Issue with github

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,36 @@
+// Image for Axxess Sentinel project
+import axxessSentinelLogin from '../components/Projects/Axxess Sentinel Login.png';
+import axxessSentinelFeature1 from '../components/Projects/Axxess Sentinel Cover.png';
+import axxessSentinelFeature2 from '../components/Projects/Axxess Sentinel Cover-2.png';
+
+// Image for Iris project
+import irisPage from '../components/Projects/Iris-Page.png';
+import irisFeature1 from '../components/Projects/Feature1-EyeDiseaseDetection.png';
+import irisFeature2 from '../components/Projects/Feature2-EyeExam.png';
+import irisFeature3 from '../components/Projects/Feature3-IrisPersonalizeReport.png';
+
+
 export const projects = [
   {
     slug: 'axxess-sentinel',
     name: 'Axxess Sentinel',
     year: '2026',
     category: 'Axxess Hackathon',
+    coverImages: [
+      {
+        src: axxessSentinelLogin,
+        alt: 'Axxess Sentinel login page preview',
+      },
+      {
+        src: axxessSentinelFeature1,
+        alt: 'Feature 1: Show current and predicted vitals',
+      },
+      {
+        src: axxessSentinelFeature2,
+        alt: 'Feature 2: Chatbot',
+      },
+    ],
+    coverAlt: 'Axxess Sentinel dashboard preview',
     summary: 'Wearable vitals to early risk alerts for patients and caregivers.',
     description:
       'Axxess Sentinel turns live wearable vitals into early risk alerts and clear forecasts, helping patients and caregivers intervene sooner with secure, role-based care workflows.',
@@ -21,11 +48,35 @@ export const projects = [
     slug: 'HACKAI',
     name: 'Iris',
     year: '2026',
-    category: 'HACKAI 2026',
+    category: 'HackAI',
+      coverImages: [
+      {
+        src: irisPage,
+        alt: 'Iris project page preview',
+      },
+      {
+        src: irisFeature1,
+        alt: 'Feature 1: Eye disease detection',
+      },
+      {
+        src: irisFeature2,
+        alt: 'Feature 2: Eye exam',
+      },
+      {
+        src: irisFeature3,
+        alt: 'Feature 3: Personalized report',
+      },
+      ],
+    coverAlt: 'Iris project preview',
     summary: 'Personalized AI-powered tools suggest people care about their eyes',
     description: `We built an application with three main features: 
-    1) Classified external eye disease that one might have by uploading a photo of their eye. The Gemini 2.5 Flask multi-modal model is used for this classification task. 
-    ,
+    1) Classified external eye disease that one might have by uploading a photo of their eye. The Gemini 2.5 Flask multi-modal model is used for this classification task. Suggest user nearby eye clinic to go to.
+    2) Using computer vision with mediapipe and 2 different models to detect eyes and calculate its position to the camera. Conducting different exams based on a published research paper to estimate hypermetropic and myopia.
+    3) Iris chatbot will use those user data collected so far to provide user with a better personalized experience.
+    We use MongoDB to store user data and ElevenLab to read those user health report and to interact with user to provide them a better experience.`,
+    tech: ['cheerio','css', 'eslint','framer-motion','google-maps', 'javacript', 'mediapipe', 'mongodb', 'next.js', 'react', 'react-hook-form', 'recharts', 'tailwindcss', 'typescript','flask','gemini 2.5','elevenlab'],
+    videoEmbedUrl: 'https://youtu.be/hgrZmG0O7mU',
+    devpostUrl: 'https://devpost.com/software/iris-8sod3k?ref_content=my-projects-tab&ref_feature=my_projects',
   },
 
 ];
